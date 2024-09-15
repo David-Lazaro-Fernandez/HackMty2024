9c3ccf55d0a4c0bb3ee2406746f9969482d0ab39
Diego Rossi <dperezrossi@gmail.com>
2024-09-15 09:26:27 -0600

    Debug

--- edge/export_data.py
+++ edge/export_data.py
@@ -6,6 +6,6 @@ headers = {"Accept":"*/*", "Connection":"keep-alive"}
 
 url = "http://10.22.238.73:8000/v1/datastore/upload"
 files=[
-  ('file',('detection_logs.log', open('./detection_logs.log','rb'),'video/mp4'))
+  ('file',('detection_logs.log', open('./detection_logs.log','rb'),'text/plain'))
 ]
 response = requests.request("POST", url, headers=headers, data=payload, files=files)
